2144591fa6f16a6e136bfd3b5ec221afae43237b
valeroscode <avalero.software@gmail.com>
2024-02-23 12:21:28 -0500

    Page is blank in production. Moved the hashrouter to the app.js file

--- src/App.js
+++ src/App.js
@@ -7,24 +7,26 @@ import Signup from "./components/registrationForm";
 import PlaceContent from "./components/PlaceContent";
 import TripPlanner from "./components/Planner";
 import Results from "./components/filteredResults";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <AuthProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<LoginForm />}></Route>
-            <Route path="place" element={<PlaceContent />} />
-            <Route path="Home" element={<TravelSmart />} />
-            <Route path="trips" element={<TripsPage />} />
-            <Route path="login" element={<LoginForm />}></Route>
-            <Route path="signup" element={<Signup />}></Route>
-            <Route path="Search-Results" element={<Results />}></Route>
-            <Route path="MyTrip" element={<TripPlanner />}></Route>
-          </Routes>
-        </div>
+        <HashRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<LoginForm />}></Route>
+              <Route path="place" element={<PlaceContent />} />
+              <Route path="Home" element={<TravelSmart />} />
+              <Route path="trips" element={<TripsPage />} />
+              <Route path="login" element={<LoginForm />}></Route>
+              <Route path="signup" element={<Signup />}></Route>
+              <Route path="Search-Results" element={<Results />}></Route>
+              <Route path="MyTrip" element={<TripPlanner />}></Route>
+            </Routes>
+          </div>
+        </HashRouter>
       </AuthProvider>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HashRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
