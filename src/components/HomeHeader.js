dd391d99d7e7960aa66a5629315352b9f08833ec
Alex Valero <avalero.software@gmail.com>
2024-06-07 10:48:43 -0400

    recoded the homeheader. Currently working on the account portion of the header. Still need to ensure it's mobile friendly.

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,17 +3,19 @@ import React, { useRef, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPaperPlane,
-  faPlaneDeparture,
   faMagnifyingGlassLocation,
-  faUser
+  faUser,
+  faArrowRightFromBracket
 } from "@fortawesome/free-solid-svg-icons";
 import { allPlaces } from "./allMarkers.mjs";
 import { useAuth } from "./contexts/AuthContext";
+import { doc } from "firebase/firestore/lite";
 
 function HomeHeader({ name }) {
   const account = useRef();
+  const editUser = useRef();
   const { currentUser, logout } = useAuth();
-  const [places_InCity, setPlaces_InCity] = useState([]);
+  const [placesDropDown, setPlacesDropDown] = useState([]);
   const [cities, setCities] = useState([]);
 
   useEffect(() => {
@@ -24,13 +26,13 @@ function HomeHeader({ name }) {
     setTimeout(() => {
       document.getElementById("users-name").style.opacity = 1;
     }, 500);
-    if (window.location.pathname === "/#/home") {
+   
       window.addEventListener("click", (e) => {
         setTimeout(() => {
-          searchBar.handleClicksOutside_ofInputs(e);
+          searchBarFunctions.handleClicksOutside_ofInputs(e);
         }, 200);
       });
-    }
+    
   }, []);
 
   function handleLogout() {
@@ -41,6 +43,36 @@ function HomeHeader({ name }) {
   const searchInput = useRef();
   const cityDD = useRef();
   const searchDD = useRef();
+
+  const searchBarFunctions = {
+    setDropdownContent: function (e) {
+      const array = [];
+      for (let i = 0; i < allPlaces.length; i++) {
+        if (allPlaces[i].city === cityInput.current.value) {
+        if (!array.includes(allPlaces[i].style)) {
+          array.push(allPlaces[i].style)
+        } else if (!array.includes(allPlaces[i].category)) {
+          array.push(allPlaces[i].category)
+        } else if (!array.includes(allPlaces[i].area)) {
+          array.push(allPlaces[i].area)
+        }
+      }
+      }
+      setPlacesDropDown(array)
+    },
+    handleClicksOutside_ofInputs: function (e) {
+      if (e.target !== searchInput.current && e.target !== cityInput.current) {
+        if (cityDD && searchDD) {
+          cityDD.current.style.display = "none";
+          searchDD.current.style.display = "none";
+        }
+      } else if (e.target !== editUser && editUser.current.style.display === "flex") {
+        editUser.current.style.display = "none";
+      } else {
+        return;
+      }
+    },
+  }
 
   const searchBar = {
     handleRenderingDropdown: function (e) {
@@ -104,63 +136,100 @@ function HomeHeader({ name }) {
   return (
     <>
       <div id="home-title">
-        <div id="home-h1">
+        <div id="home-org">
+      <div id="home-h1">
           <FontAwesomeIcon icon={faPaperPlane} className="plane" />
           <h1>TRAVEL SMART</h1>
         </div>
-        <div id="homesearch">
-          <div
-            className="first-input"
-            onClick={(e) => searchBar.handleRenderingDropdown(e)}
-          >
-            <div id="search-bar-wrapper">
-              <div id="place-search-div">
-                <input
-                  autoComplete="off"
-                  type="text"
-                  ref={searchInput}
-                  id="city-place-search"
-                  placeholder={
-                    window.innerWidth > 1300
-                      ? "Resturant, miami beach, etc."
-                      : "Sushi, Coffee, etc."
-                  }
-                />
-                <div
-                  id="place-search-DD"
-                  ref={searchDD}
-                  onClick={(e) => searchBar.handleDropdownClicks(e)}
-                >
-                  {places_InCity.map((place) => (
-                    <p className="place city">{place}</p>
-                  ))}
-                </div>
-              </div>
-              <div id="city-search-div">
-                <input
-                  autoComplete="off"
-                  type="text"
-                  ref={cityInput}
-                  id="place-search"
-                  placeholder="Choose your city"
-                />
-                <div
-                  id="city-search-DD"
-                  ref={cityDD}
-                  onClick={(e) => searchBar.handleDropdownClicks(e)}
-                >
-                  {cities.map((city) => (
-                    <p className="place city">{city}</p>
-                  ))}
-                </div>
-              </div>
 
-              <div className="home-search-btn" onClick={searchPlaces}>
-                <FontAwesomeIcon icon={faMagnifyingGlassLocation} />
-              </div>
+        <div id="home-searchbar">
+        <div id="cityDD-input">
+          <input ref={cityInput} placeholder="Choose A City" onClick={() => {
+            cityDD.current.style.display = "flex"
+          }} onKeyUp={(e) => {
+             const city = document.getElementsByClassName("city-dd-item")
+             const inputValue = String(e.target.value).toLocaleLowerCase();
+             for (let i = 0; i < city.length; i++) {
+              
+              if (e.target.value === "") {
+                city[i].style.display = "flex"
+              } else {
+             if (String(city[i].textContent).toLocaleLowerCase().includes(inputValue)) {
+              city[i].style.display ="block"
+             } else {
+              city[i].style.display = "none"
+             }
+            }
+            }
+          }}></input>
+          <ul ref={cityDD}>
+            {
+              cities.map((city) => (
+                <li className="city-dd-item" onClick={(e) => {
+                  cityInput.current.value = e.target.textContent;
+                  cityDD.current.style.display = "none"
+                  searchBarFunctions.setDropdownContent()
+                }}>{city}</li>
+              ))
+            }
+          </ul>
+          </div>
+          <div id="placeDD-input">
+          <input ref={searchInput} placeholder="Sushi, Bars, Area"
+          onClick={() => {
+            searchDD.current.style.display = "flex"
+          }}
+          onKeyUp={(e) => {
+            const place = document.getElementsByClassName("place-dd-item")
+            const inputValue = String(e.target.value).toLocaleLowerCase();
+            for (let i = 0; i < place.length; i++) {
+             if (e.target.value === "") {
+              place[i].style.display = "none"
+              searchDD.current.style.display = "none"
+             } else {
+              
+            if (String(place[i].textContent).toLocaleLowerCase().includes(inputValue)) {
+              searchDD.current.style.display = "flex"
+              place[i].style.display ="block"
+            } else {
+              place[i].style.display = "none"
+            }
+           }
+           }
+         }}></input>
+          <ul ref={searchDD}>
+            {
+              placesDropDown.map((content) => (
+                <li className="place-dd-item" onClick={(e) => {
+                  searchInput.current.value = e.target.textContent;
+                  searchDD.current.style.display = "none"
+                  searchInput.current.value = e.target.textContent
+                }}>
+                  {content}
+                </li>
+              ))
+            }
+          </ul>
+          </div>
+          
+          <button onClick={searchPlaces}><FontAwesomeIcon icon={faMagnifyingGlassLocation} /></button>
+        </div>
+        <div id="acc-and-trip">
+          
+        <div ref={account} className="account" 
+        onClick={() => {
+          editUser.current.style.display = "flex";
+        }}>
+          <div ref={editUser} id="edit-user">
+            <div>
+            <FontAwesomeIcon icon={faUser} />
+            <h4>{name}</h4>
             </div>
-            <div id="acc-and-trip">
-              <div ref={account} className="account">
+            <h5>avalero.software@gmail.com</h5>
+            <hr/>
+
+            <button><FontAwesomeIcon icon={faArrowRightFromBracket} /> Sign Out</button>
+          </div>
                 {currentUser ? (
                   <div className="user-name">
                     <p id="users-name">{name}</p>
@@ -173,8 +242,7 @@ function HomeHeader({ name }) {
                 )}
               </div>
             </div>
-          </div>
-        </div>
+      </div>
       </div>
     </>
   );
